fe0edf6a923a017c5547a9f16204d4b74627665c
Ernst Bekkering <bekkerin@users.noreply.github.com>
2026-07-14 17:18:09 -0500

    Initial commit

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public void StartPlay()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void StopPlaying()
+    {
+        SceneManager.LoadScene("Exit");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void ExitGame()
+    {
+        // exit the game for real play
+        Application.Quit();
+        //does not work in Unity since it would  lose all settings. so:
+        //UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
